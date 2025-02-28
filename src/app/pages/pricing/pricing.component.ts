38e6ef7f5d0b38ef24a9bc93dac99780fa5f5a38
Dantee Astudillo <dantee-mauri@hotmail.com>
2025-02-27 19:04:49 -0500

    Creación de pages y navbar

---
+++ src/app/pages/pricing/pricing.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing',
+  imports: [],
+  templateUrl: './pricing.component.html',
+  styleUrl: './pricing.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PricingComponent { }
