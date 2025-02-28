13b189b60084424cfee697a08d182391c6f9c49e
Dantee Astudillo <dantee-mauri@hotmail.com>
2025-02-27 20:09:35 -0500

    Se agg lógica para cambiar title de cada página y actualizar metatags

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
@@ -7,4 +8,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './contact.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export  default class ContactComponent { }
+export default class ContactComponent {
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('Contact Page');
+    this.meta.updateTag({name:'description',content:'este es mi contact page'});
+    this.meta.updateTag({name:'og:title',content:'este es mi contact page'});
+    this.meta.updateTag({name:'keywords',content:'pricing page, SSR'});    
+  }
+
+}
--- src/app/pages/pricing/pricing.component.ts
+++ src/app/pages/pricing/pricing.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pricing',
@@ -7,4 +9,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './pricing.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingComponent { }
+export default class PricingComponent {
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject((PLATFORM_ID));
+
+    ngOnInit(): void {
+      //Ejecutar código del lago del cliente.
+      if(isPlatformBrowser(this.platform)){
+        // document.title = 'pricing';
+      }
+      this.title.setTitle('Pricing Page');
+      this.meta.updateTag({name:'description',content:'este es mi pricing page'});
+      this.meta.updateTag({name:'og:title',content:'este es mi pricing page'});
+      this.meta.updateTag({name:'keywords',content:'pricing page, SSR'});    
+    } 
+ }
