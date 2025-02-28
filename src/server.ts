8ceb298f44666a2a14b2cf0ed769e81532791726
Dantee Astudillo <dantee-mauri@hotmail.com>
2025-02-28 12:01:09 -0500

    Se mueve archivo de netlify a ruta base

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,8 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine, isMainModule } from '@angular/ssr/node';
-import express from 'express';
-import { dirname, join, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import bootstrap from './main.server';
+import { CommonEngine, } from '@angular/ssr/node';
 import { render } from '@netlify/angular-runtime/common-engine'
 
-const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-const browserDistFolder = resolve(serverDistFolder, '../browser');
-const indexHtml = join(serverDistFolder, 'index.server.html');
 
-const app = express();
 const commonEngine = new CommonEngine();
-
-
 
 export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
   try {    
